966415372f2b2e2919ac24a02c609bedc10cbe48
Conner Yoon <con.yoon@icloud.com>
2022-05-26 15:50:34 -0400

    Initial Commit

---
+++ Mercy Or Evil/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mercy Or Evil
+//
+//  Created by Conner Yoon on 5/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mercy Or Evil/Mercy_Or_EvilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mercy_Or_EvilApp.swift
+//  Mercy Or Evil
+//
+//  Created by Conner Yoon on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Mercy_Or_EvilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
