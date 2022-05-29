fb998ed1d77efd59228d76f51849a59383e62deb
Conner Yoon <con.yoon@icloud.com>
2022-05-29 19:10:06 -0400

    boredom and hostility added, images for mercy and evil

--- Mercy Or Evil/ContentView.swift
+++ Mercy Or Evil/ContentView.swift
@@ -18,22 +18,33 @@ struct ContentView: View {
     @State var evilCount = 0
     @State var mercyCount = 0
     let painNoises = [ "Ooof! ", "Reee! ", "Agggh! ", "Grk! ", "Help! ", "Mercy Please!", "OW!", "AHHH THE PAIN!", "STOP IT!", "@!#$@$##@$!", "Who do you think you are?!"]
-    let idle = ["Whoever you are don't touch the buttons.  It's better for the both of us.", "I can't help but feel I know you.  Who are you?", "Is this thing on?  I can't hear you.", "I keep forgetting. Why am I here?", "Ugh, man this place is cramped", "I'm having some desha fu.  I swear I've seen this place before.", "Who are you?", "Sup", "Is this thing on?  I can't hear you.", "...", "Well, I guess this is better than other things happening", "What are you doing?", "What's your name?", "Is something happening there?", "I swear I am repeating myself, but I can't help it"]
-    let wary = ["...", "okay", "thanks... I guess"]
+    let idle = ["Whoever you are don't touch the buttons.  It's better for the both of us.", "I can't help but feel I know you. Who are you?", "I keep forgetting. Why am I here?", "I'm having some desha fu. I swear I've seen this place before.", "Who are you?", "Sup", "...", "What's your name?", "Is something happening there?", "I swear I am repeating myself, but I can't help it"]
+    let bored = ["Helloooo?", "Is this thing on?  I can't hear you.", "uggh, I'm getting bored. Want to play a game?", "Silent type, I get it.", "Ugh, man this place is cramped", "What the heck am I doing here?"]
+    let wary = ["...", "okay", "Well, I guess this is better than other things happening","What are you doing?", "thanks... I guess"]
     let hostile =  ["sus", "I'm watching you", "*intense glare*", "I know you're thinking about it."]
     let suprise = ["Wah!", "Noo-!", "Oh Go-!", "Ga-!", "Oh I swear-!", "Why?!"]
     @State var buttonlabel : String
-    
+    @State var imageName = ""
     var body: some View {
         ZStack {
+            
             //            LinearGradient(gradient: Gradient(colors: [.black, .blue]), startPoint: .top, endPoint: .bottom).ignoresSafeArea()
             //            Image("Skull")
             //                .resizable()
             //                .frame(width: CGFloat(10.0) * CGFloat(screamCount), height: CGFloat(10.0) * CGFloat(screamCount))
             //
             //                .clipShape(Circle())
-            HStack{
+//            HStack{
                 VStack{
+                    VStack{
+                        Image(imageName)
+                            .resizable()
+        //                    .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .frame(width: 350, height: 350)
+                            
+                        Spacer()
+                    }
                     VStack{
                         //                    Text("Screams \(screamCount)")
                         Text("\(buttonlabel)").font(fontsize).foregroundColor(color).bold().padding(.bottom, 80)
@@ -80,7 +91,7 @@ struct ContentView: View {
                 }.padding()
             }
             
-        }
+//        }
         
     }
     init(){
@@ -91,6 +102,7 @@ struct ContentView: View {
 
 extension ContentView {
     func handleEvilButtonPress() {
+        imageName = "Hurt"
         evil = true
         color = .red
         fontsize = .title
@@ -106,13 +118,24 @@ extension ContentView {
     }
     
     func handleMercyButtonPress(){
+        imageName = "Joy"
         switch screamCount {
         case 0:
             if evil {
+                if evilCount<5{
                 buttonlabel = wary[Int.random(in: 0..<wary.count)]
+                }
+                else{
+                    buttonlabel = hostile[Int.random(in: 0..<hostile.count)]
+                }
             }
             else{
+                if mercyCount < 10 {
             buttonlabel = idle[Int.random(in: 0..<idle.count)]
+                }
+                else {
+                    buttonlabel = bored[Int.random(in: 0..<bored.count)]
+                }
             }
         case 1...3:
             buttonlabel = "Phew."
