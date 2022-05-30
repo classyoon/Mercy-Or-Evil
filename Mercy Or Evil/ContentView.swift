db68783c5ad45bda8ace6c0d3f68eeba689a172a
Conner Yoon <con.yoon@icloud.com>
2022-05-30 12:56:11 -0400

    Added in grudge mechanic

--- Mercy Or Evil/ContentView.swift
+++ Mercy Or Evil/ContentView.swift
@@ -12,32 +12,28 @@ import SwiftUI
 struct ContentView: View {
     @State var evil = false
     @State var mercy = true;
+    @State var mood = ""
     @State var color = Color.primary
     @State var fontsize = Font.body
-    @State var screamCount = 0
+    @State var painCount = 0
     @State var evilCount = 0
     @State var mercyCount = 0
+    @State var waryCount = 0
+    @State var hostileCount = 0
     let painNoises = [ "Ooof! ", "Reee! ", "Agggh! ", "Grk! ", "Help! ", "Mercy Please!", "OW!", "AHHH THE PAIN!", "STOP IT!", "@!#$@$##@$!", "Who do you think you are?!"]
     let idle = ["Whoever you are don't touch the buttons.  It's better for the both of us.", "I can't help but feel I know you. Who are you?", "I keep forgetting. Why am I here?", "I'm having some desha fu. I swear I've seen this place before.", "Who are you?", "Sup", "...", "What's your name?", "Is something happening there?", "I swear I am repeating myself, but I can't help it"]
     let bored = ["Helloooo?", "Helloooo?", "Helloooo?", "Is this thing on?  I can't hear you.", "I keep forgetting. Why am I here?", "uggh, I'm getting bored. Want to play a game?", "Silent type, I get it.", "Ugh, man this place is cramped", "What the heck am I doing here?", "I'm having some desha fu. I swear I've seen this place before.", "Wanna talk?"]
     let wary = ["...", "okay", "Well, I guess this is better than other things happening","What are you doing?", "thanks for stopping... I guess", "*cowers in fear*", "At least it stopped"]
-    let hostile =  ["sus", "I'm watching you", "*intense glare*", "I know you're thinking about it.", "I know you're thinking about it.", "thanks... I guess", "*cowers in fear*", "*cowers in fear*", "Imma get you", "You know not what you do"]
+    let hostile =  ["sus", "I'm watching you", "*intense glare*", "I know you're thinking about it.", "I know you're thinking about it.", "thanks... I guess", "*cowers in fear*", "*cowers in fear*", "Imma get you", "You know not what you do", "You seem untrustworthy"]
     let suprise = ["Wah!", "Noo-!", "Oh Go-!", "Ga-!", "Why?!"]
-//    let resigned = ["Wah!", "Noo-!", "Oh Go-!", "Ga-!", "Oh I swear-!", "Why?!", "Not again!"]
+    let resigned = ["Wah!", "Noo-!", "Oh Go-!", "Ga-!", "Oh I swear-!", "Why?!", "Not again!", "Not again!", "Don't"]
     let relief = ["Phew", "No more. okay?", "Why did you do that?", "Never again"]
     let fear = ["Why did you do that?", "Thank goodness you stopped", "*gasp* I can breath again", "*cowers in fear*",  "*cowers in fear*"]
     @State var buttonlabel : String
     @State var imageName = ""
     var body: some View {
         ZStack {
-            
-            //            LinearGradient(gradient: Gradient(colors: [.black, .blue]), startPoint: .top, endPoint: .bottom).ignoresSafeArea()
-            //            Image("Skull")
-            //                .resizable()
-            //                .frame(width: CGFloat(10.0) * CGFloat(screamCount), height: CGFloat(10.0) * CGFloat(screamCount))
-            //
-            //                .clipShape(Circle())
-//            HStack{
+
                 VStack{
                     VStack{
                         Image(imageName)
@@ -49,7 +45,8 @@ struct ContentView: View {
                         Spacer()
                     }
                     VStack{
-                        //                    Text("Screams \(screamCount)")
+                        //                    Text("Screams \(painCount)")
+                                           Text("Mood : \(mood), Grudge : \(hostileCount)")
                         Text("\(buttonlabel)").font(fontsize).foregroundColor(color).bold().padding(.bottom, 80)
                     }
                     HStack{
@@ -109,50 +106,75 @@ extension ContentView {
         evil = true
         color = .red
         fontsize = .title
-        screamCount+=1
+        hostileCount+=2
+        painCount+=1
         evilCount+=1
         buttonlabel=painNoises[Int.random(in: 0..<painNoises.count)]
+        mood = "in pain"
+       
         if mercy {// If you were nice before then suddenly evil you will get suprise
+            if (evilCount>2){//If you were evil in the past then merciful
+                buttonlabel = resigned[Int.random(in: 0..<resigned.count)]
+                mercyCount = 0
+                mercy = false
+                mood = "exasperated"
+            }
+            else{
             buttonlabel = suprise[Int.random(in: 0..<suprise.count)]
             mercyCount = 0
             mercy = false
+                mood = "suprised"
+                hostileCount+=5
+            }
         }
        
     }
     
     func handleMercyButtonPress(){
         imageName = "Joy"
-        switch screamCount {
+        switch painCount {
         case 0:
             if evil { //If you have been mean
-                if evilCount<15{
+                if evilCount<15||hostileCount==0{
                 buttonlabel = wary[Int.random(in: 0..<wary.count)]
+                    mood = "wary"
                 }//at first wary
                 else{
                     buttonlabel = hostile[Int.random(in: 0..<hostile.count)]//then hostile
+                    mood = "hostile"
+                    hostileCount-=1
                 }
             }
             else{//if you haven't been mean
                 if mercyCount < 20 {
             buttonlabel = idle[Int.random(in: 0..<idle.count)]//at first idle
+                    mood = "idle"
                 }
                 else {
                     buttonlabel = bored[Int.random(in: 0..<bored.count)]//then bored
+                    mood = "bored"
                 }
             }
-        case 1...3:
+        case 1...8:
+            if evilCount < 5 {
             buttonlabel = relief[Int.random(in: 0..<relief.count)]
-        case 4...8:
+                mood = "relief"
+            }
+            else {
             buttonlabel = fear[Int.random(in: 0..<fear.count)]
-        case 9...15:
+                mood = "fear"
+            }
+        case 9...25:
             buttonlabel = "OMG. I thought you'd never stop.  Please now.  Don't touch do whatever you did, again"
+            mood = "big relief"
         default:
             buttonlabel = "REALLY?"
+            mood = "exasperated (this seemed excessive)"
         }
         mercy = true
         color = .green
         fontsize = .body
-        screamCount = 0
+        painCount = 0
         mercyCount+=1
     }
 }
